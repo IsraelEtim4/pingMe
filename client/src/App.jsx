0097f4e507cdb3cd6d96e49e98f104e57fa2aded
Etim Israel <israeletim4@gmail.com>
2025-07-31 07:55:51 +0100

    client installations ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from "react-router";
+
+import HomePage from './pages/HomePage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from "./pages/ChatPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div>
-      Welcome to PingMe App
+    <div className="">
+      <div className="my-10">
+        Welcome to PingMe App
+      </div>
+      <div>
+        <button className="btn" onClick={() => toast.success("PingMe!")}>Button</button>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+
+      <Toaster />
     </div>
   )
 }
